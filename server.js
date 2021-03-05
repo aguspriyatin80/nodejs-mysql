f02793be78e58ae1ce8613c3d3c61f48873d0326
Agus Priyatin <aguspriyatin80>
2021-03-05 13:29:55 +0700

    membuat controller.js

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const app = express(); // memanggil express secara global
 //parse application/json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+//panggil routes
+var routes = require('./routes');
+routes(app);
+
 //menentukan port
 app.listen(3000, () => {
     console.log(`Server started on port`);
